21cf7e15fcabdfb1fbe97bd5a1451bf6c3cbf7b7
Naufal Zaky Mubarak <naufalzakymubarok@gmail.com>
2024-05-12 17:12:50 +0700

    Push Sisa
    Push Sisa

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//13:27 https://youtu.be/JF9Zbxu8Mz0?si=OtFkSUyFJPrNcUbr
+//npm run dev - run ReactJS
