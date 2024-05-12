8cbc2fa1daa07efe0b17c1ca1acffecce4a6af7f
Naufal Zaky Mubarak <naufalzakymubarok@gmail.com>
2024-05-12 17:12:33 +0700

    Push SRC
    Push SRC

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import React from "react";
+//import FormLogin from "./components/Fragments/FormLogin";
+//import LoginPage from "./pages/login";<LoginPage></LoginPage>
+import RegisterPage from "./pages/register";
+
+function App() { 
+  return (
+    <div className="flex justify-center min-h-screen items-center">
+      
+      <RegisterPage></RegisterPage>
+  
+    </div>
+    
+  );
+}
+
+export default App;
+  //18:02 - Tutorial React JS Bahasa Indonesia : 5.Implementasi Atomic Design
